47ffa210a586b896b813b552865a1b836a4e5c56
ZoeYou <zuoyouwk@gmail.com>
2022-06-14 15:21:13 +0200

    upload

---
+++ get_SAO_en.py
@@ -0,0 +1,53 @@
+import spacy 
+
+nlp = spacy.load('en_core_web_sm')
+stopwords = nlp.Defaults.stop_words
+
+
+def get_SAO_en(sentence, model=nlp):
+    doc = model(sentence)
+    res = []
+
+    # get all noun trunks 
+    nouns = [n for n in doc.noun_chunks]
+
+    # filter for dobj
+    nouns_dobj = [n for n in nouns if n.root.dep_=="dobj"]
+
+    for n in nouns_dobj:
+        object = " ".join([w.text for w in n if w.pos_!="DET"])
+        verb = n.root.head.lemma_
+
+        subject = None
+        # situation 1: acl 
+        if n.root.head.dep_ == "acl":
+            subject = n.root.head.head
+            while subject.dep_ == "acl":
+                subject = subject.head
+        
+        # situation 2: nsubj
+        else:
+            subject_node = [child for child in n.root.head.children if child.dep_=="nsubj"]
+            if subject_node: subject = subject_node[0]
+        
+        # find noun trunk that includes the subject noun
+        if subject:
+            try:
+                subject = [n for n in nouns if subject in n][0]
+                subject = " ".join([w.text for w in subject if w.pos_!="DET"])
+            except IndexError:
+                subject = subject.text
+
+            if (subject not in stopwords and object not in stopwords) and (subject!=object):
+                res.append((subject, verb, object))
+    return res
+
+############################ TEST ################################
+from nltk import sent_tokenize
+claims = "1. A display substrate, comprising: a base substrate; a thin film transistor on the base substrate; and a light shielding layer on the base substrate, the light shielding layer comprising a first light shielding layer and a second light shielding layer that are stacked, wherein an orthographic projection of an active layer of the thin film transistor on the base substrate is within an orthogonal projection of the light shielding layer on the base substrate, and the second light shielding layer comprises nanoparticles capable of absorbing light in a specific wavelength range.\n2. The display substrate according to claim 1, wherein a material of the first light shielding layer comprises monocrystalline silicon, polycrystalline silicon or amorphous silicon.\n3. The display substrate according to claims 1 or 2, wherein a material of the second light shielding layer further comprises silicon nitride or silicon carbide.\n4. The display substrate according to any one of claims 1-3, wherein a thickness of the first light shielding layer ranges from 400 Å to 600 Å, and a thickness of the second light shielding layer ranges from 200 Å to 500 Å.\n5. The display substrate according to any one of claims 1-4, wherein the nanoparticles are nano silicon particles.\n6. The display substrate according to claim 5, wherein particle sizes of the nano silicon particles range from 3nm to 5nm.\n7. The display substrate according to any one of claims 1-6, wherein the light is blue  light, and a wavelength of the blue light ranges from 420nm to 480 nm.\n8. The display substrate according to any one of claims 1-7, wherein the first light shielding layer is on a side of the second light shielding layer that is away from the base substrate; or the second light shielding layer is on a side of the first light shielding layer that is away from the base substrate\n9. The display substrate according to any one of claims 1-8, wherein the thin film transistor comprises a thin film transistor of a top gate structure or a thin film transistor of a bottom gate structure.\n10. The display substrate according to claim 9, wherein in a case where the thin film transistor has the bottom gate structure, the light shielding layer is on a side of the active layer that is away from the base substrate; or in a case where the thin film transistor has the top gate structure, the light shielding layer is disposed between the base substrate and the active layer.\n11. A display device, comprising the display substrate according to any one of claims 1-10.\n12. A manufacture method of a display substrate, comprising: providing a base substrate; forming a thin film transistor on the base substrate; and forming a light shielding layer, which comprises a first light shielding layer and a second light shielding layer, on the base substrate, wherein an orthographic projection of an active layer of the thin film transistor on the base substrate is within an orthogonal projection of the light shielding layer on the base substrate, and the second light shielding layer comprises nanoparticles capable of absorbing light in a specific wavelength range.\n13. The manufacture method of a display substrate according to claim 12, wherein a method of forming the second light shielding layer comprises spiral wave plasma chemical vapor deposition.\n14. The manufacture method of a display substrate according to claims 12 or 13, wherein the nanoparticles are nano silicon particles, and forming the second light shielding layer comprises: forming a second light shielding layer film comprising the nano silicon particles through a reaction gas comprising at least nitrogen, silane and hydrogen, or through a reaction gas comprising at least nitrogen, methane, silane and hydrogen; and performing a patterning process on the second light shielding layer film to form the second light shielding layer comprising the nano silicon particles.\n15. The manufacture method of a display substrate according to claim 14, wherein in a case where the second light shielding layer film is formed through the reaction gas comprising at least nitrogen, methane, silane and hydrogen, process conditions of the spiral wave plasma chemical vapor deposition comprise: a temperature ranging from 650 degrees Celsius to 750 degrees Celsius, power ranging from 400 Watts to 600 Watts, low pressure of pressure being up to 1.33 Pa, and a magnetic induction intensity ranging from 90 Gs to 130 Gs.\n16. The manufacture method of a display substrate according to claim 15, wherein the process conditions of the spiral wave plasma chemical vapor deposition comprise: the temperature being 700 degrees Celsius; the pressure is 1.33 Pa, the power being 500 watts; the magnetic induction intensity being 110 Gs and a volume ratio of the hydrogen, methane and silane being 1:2:40.\n17. The manufacture method of a display substrate according to any one of claims 12-16, wherein the light is blue light, and a wavelength of the blue light ranges from 420nm to 480  nm.\n18. The manufacture method of a display substrate according to any one of claims 12-16, wherein forming the light shielding layer, which comprises the first light shielding layer and the second light shielding layer, on the base substrate comprises: forming the first light shielding layer on the base substrate; and forming the second light shielding layer on the first light shielding layer.\n19. The manufacture method of a display substrate according to claim 18, wherein the light shielding layer is formed synchronously with the active layer in the thin film transistor, and the method comprises: after a thin film of the light shielding layer and a thin film of the active layer are sequentially formed on the base substrate, using a same mask for the thin film of the light shielding layer and the thin film of the active layer to form the light shielding layer and the active layer."
+
+sents = sent_tokenize(claims, language="english")
+for sent in sents:
+    saos = get_SAO_en(sent)
+    if saos:
+        print(saos)
---
+++ get_SAO_fr.py
@@ -0,0 +1,100 @@
+import spacy 
+
+# !python -m spacy download fr_dep_news_trf
+nlp = spacy.load('fr_dep_news_trf')
+stopwords = nlp.Defaults.stop_words
+
+
+def find_mod_subj(token, doc):
+    """
+    given the subject token, returns its noun trunk
+    """
+    idx = [token.i]
+
+    amod_tokens = [child for child in token.children if child.dep_=="amod"]
+    if amod_tokens:
+        idx.extend([t.i for t in amod_tokens])
+
+    low, high = find_mod_obj(token)
+    idx.extend([low, high])
+
+    if token.head.dep_=="nmod":
+        nmod_token = token.head
+        idx.append(nmod_token.i)
+        while nmod_token.dep_=="nmod":
+            nmod_token = nmod_token.head
+            idx.append(nmod_token.i)
+    return doc[min(idx): max(idx)+1]
+
+def find_mod_obj(token):
+    """
+    given the object token, returns it noun trunk
+    """
+    idx = [token.i]
+    amod_tokens = [child for child in token.children if child.dep_=="amod"]
+    if amod_tokens:
+        idx.extend([t.i for t in amod_tokens])
+
+    nmod_tokens = [child for child in token.children if child.dep_=="nmod"]
+    if nmod_tokens:
+        for t in nmod_tokens:
+            index_low, index_high = find_mod_obj(t)
+        idx.extend([index_low, index_high])
+
+    return min(idx), max(idx)
+
+def get_SAO_fr(sentence, model=nlp):
+    doc = model(sentence)
+    res = []
+
+    # filter for obj
+    nouns_obj = [n for n in doc if n.dep_=="obj"]
+
+    for n in nouns_obj:
+        if n.head.pos_ == "VERB":
+            verb = n.head
+            object = n 
+        else:
+            continue
+
+        subject = None
+        # situation 1: acl 
+        if n.head.dep_ == "acl":
+            subject = n.head.head
+            while subject.dep_ == "acl":
+                subject = subject.head
+
+        # situation 2: nsubj
+        else:
+            subject_node = [child for child in n.head.children if child.dep_=="nsubj"]
+            if subject_node: subject = subject_node[0]
+        
+        # find noun trunk that includes the subject noun
+        if subject:
+            subject_trunk = find_mod_subj(subject, doc)
+            
+            obj_start, obj_end = find_mod_obj(object)
+            object_trunk = doc[obj_start: obj_end+1]
+            res.append((subject_trunk.text, verb.lemma_, object_trunk.text))
+
+            # check if object has another conjunct object
+            conj_tokens = [child for child in object_trunk.root.children if child.dep_=="conj"]
+            if conj_tokens:
+                for t in conj_tokens:
+                    obj_start, obj_end = find_mod_obj(t)
+                    object_trunk = doc[obj_start: obj_end+1]
+                    res.append((subject_trunk.text, verb.lemma_, object_trunk.text))
+        else:
+            continue
+    return res
+
+############################ TEST ################################
+from nltk import sent_tokenize
+
+claims = "REVENDICATIONS1. Conduite de transport d'un liquide lave-glace pour véhicule automobile comportant au moins un canal de circulation du liquide lave glace destiné à s'étendre depuis un réservoir de liquide lave glace jusqu'à un balai d'essuie-glace, caractérisée en ce qu'elle comporte d'au moins un élément filaire conducteur électrique (20,25,50,75) comportant au moins une âme électrique centrale (21,26,51,76) et un brin électrique coaxial (23,28,53,78) qui est isolé électriquement de la dit âme centrale (21,26,51,76). 2. Conduite selon la revendication 1, caractérisée en ce que le brin électrique coaxial (23,28,53,78) est bobiné en hélice autour d'un manchon électriquement isolant (22,27,52,77) entourant l'âme électrique centrale (21,26,51,76). 3. Conduite selon l'une quelconque des revendications 1 à 2, caractérisée en ce que l'âme centrale électrique (21,26,51,76) est tressée. 4. Conduite selon l'une quelconque des revendications 1 et 2, caractérisée en ce que l'élément filaire conducteur électrique (20,25,50,75) comporte une gaine extérieure isolante (24,29,54,79). 5. Conduite selon l'une quelconque des revendications 1 à 4, caractérisée en ce qu'elle comporte un premier élément filaire conducteur électrique (20) qui assure l'alimentation électrique d'un dispositif de chauffe (40,68) d'un balai d'essuie-glace, dont une première extrémité (37) est alimentée en électricité et dont l'extrémité opposée (39) est reliée électriquement au balai d'essuie-glace. 6. Conduite selon l'une quelconque des revendications 1 à 4, caractérisée en ce qu'elle comporte un deuxième élément filaire conducteur électrique (25) qui est chauffant, qui assure le chauffage du liquide lave glace circulant dans le canal de circulation (32 ;35,36), dont une première extrémité (42) est alimentée en électricité et dont l'extrémité opposée (43) comporte des moyens (44) pour relier 3039483 12 électriquement l'âme centrale (26) au brin coaxial (28) en formant une boucle de courant. 7. Conduite selon l'une quelconque des revendications 1 à 4, caractérisée en 5 ce qu'elle comporte un premier élément filaire conducteur électrique (20) qui assure l'alimentation électrique d'un dispositif de chauffe du balai d'essuie-glace, dont une première extrémité (37) est alimentée en électricité et dont l'extrémité opposée (39) alimente le dispositif de chauffe du balai d'essuie-glace, et en ce qu'elle comporte en outre un deuxième élément filaire conducteur électrique (25) 10 qui est chauffant, qui assure le chauffage du liquide lave glace circulant dans le canal de circulation, dont une première extrémité (42) est alimentée en électricité et dont l'extrémité opposée (43) comporte des moyens pour relier l'âme centrale (26) au brin coaxial (28) en formant une boucle de courant. 15 8. Conduite selon l'une quelconque des revendications 1 à 4, caractérisée en ce qu'elle comporte une paire de troisièmes éléments filaires conducteurs électriques (50,50a,50b) dont le brin coaxial électrique (53,53a) est chauffant, en ce que les brins coaxiaux électriques chauffants (53a,53b) desdits troisièmes éléments filaires (50a,50b) sont d'une part alimentés électriquement et d'autre 20 part connectés entre eux pour former une boucle de courant et assurer le chauffage du liquide lave glace circulant dans le canal de circulation (57 ;62,63), et en ce que l'âme électrique centrale (51a) de chacun des troisièmes éléments filaires (50a,50b) est d'une part alimentée électriquement et d'autre part électriquement connectée à un dispositif de chauffe du balai d'essuie-glace. 25 9. Conduite selon l'une quelconque des revendications 1 à 4, caractérisée en ce qu'elle comporte une paire de quatrièmes éléments filaires conducteurs électriques (75) dont l'âme électrique centrale (78) est chauffante, en ce que les âmes électriques centrales chauffantes (78) des quatrièmes éléments filaires (75) 30 sont respectivement d'une part alimentées électriquement et d'autre part connectées entre elles pour former une boucle de courant et assurer le chauffage du liquide lave glace circulant dans le canal de circulation (57 ;62,63), et en ce que le brin coaxial électrique (76) de chacun des quatrièmes éléments filaires 3039483 13 (75) est d'une part alimenté électriquement et d'autre part électriquement connecté à un dispositif de chauffe du balai d'essuie-glace. 5 10. Conduite selon l'une quelconque des revendications précédentes, caractérisée en ce qu'elle comporte un manchon extrudé (31,34,56,61,46) comportant au moins un canal de circulation du liquide lave glace (32 ;35,36 ;57 ;62,63 ;47,48), et en ce qu'au moins un élément filaire conducteur électrique (20 ;25 ;50,75) est noyé dans la masse du dit manchon 10 (31,34,56,61,46) . 11. Conduite selon l'une quelconque des revendications précédentes, caractérisée en ce qu'elle comporte un canal de circulation de liquide lave-glace (32,57) et en ce que des éléments filaires conducteurs électriques 15 (20,50,50a,50b,75) sont disposés de part et d'autre du dit canal de circulation (32,57). 12. Conduite selon l'une quelconque des revendications 1 à 10, caractérisée en ce qu'elle comporte deux canaux de circulation (35,36 ;62,63 ;47,48) pour 20 alimenter un balai d'essuie-glace à deux rampes d'arrosage. 13. Conduite selon la revendication 12, caractérisé en ce que les élément filaires conducteurs électriques (25,50,50a,50b,75) sont disposés entre les deux canaux de circulation (35,36 ;62,63). 25 14.Dispositif d'essuyage pour surface vitrée de véhicule automobile comportant au moins un balai d'essuie-glace muni d'au moins une première rampe d'arrosage, caractérisé en ce qu'il comporte une conduite de transport d'un liquide lave glace (30,33,55,60,45) selon l'une quelconque des revendications 1 à 30 13, raccordée à ladite première rampe d'arrosage. 15. Dispositif d'essuyage pour surface vitrée de véhicule automobile comportant au moins un balai d'essuie-glace muni d'une première et d'une seconde rampe d'arrosage de chaque côté du bras du balai d'essuie-glace, 3039483 -14 caractérisé en ce qu'il comporte une conduite de transport d'un liquide lave glace (33,60,45) selon l'une quelconque des revendications 12 et 13."
+
+sents = sent_tokenize(claims, language="french")
+for sent in sents:
+    saos = get_SAO_fr(sent)
+    if saos:
+        print(saos)
